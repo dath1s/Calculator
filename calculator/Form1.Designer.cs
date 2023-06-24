adc723035c82aa11096884626d047d13766e5299
dmitry <kovtunov.da@students.dvfu.ru>
2023-06-25 02:28:34 +1000

    Добавьте файлы проекта.

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,456 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            memory_clean_button = new Button();
+            memory_read_button = new Button();
+            memory_save_button = new Button();
+            memory_add_button = new Button();
+            memory_sub_button = new Button();
+            erase = new Button();
+            clean_cur_num = new Button();
+            clean_all = new Button();
+            sign = new Button();
+            sqrt = new Button();
+            seven = new Button();
+            eight = new Button();
+            nine = new Button();
+            div = new Button();
+            percent = new Button();
+            four = new Button();
+            five = new Button();
+            six = new Button();
+            mul = new Button();
+            button20 = new Button();
+            one = new Button();
+            two = new Button();
+            three = new Button();
+            sub = new Button();
+            point = new Button();
+            add = new Button();
+            eq = new Button();
+            zero = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // memory_clean_button
+            // 
+            memory_clean_button.BackColor = SystemColors.GradientActiveCaption;
+            memory_clean_button.Location = new Point(8, 103);
+            memory_clean_button.Name = "memory_clean_button";
+            memory_clean_button.Size = new Size(45, 30);
+            memory_clean_button.TabIndex = 1;
+            memory_clean_button.Text = "MC";
+            memory_clean_button.UseVisualStyleBackColor = false;
+            memory_clean_button.Click += button1_Click;
+            // 
+            // memory_read_button
+            // 
+            memory_read_button.BackColor = SystemColors.GradientActiveCaption;
+            memory_read_button.Location = new Point(59, 103);
+            memory_read_button.Name = "memory_read_button";
+            memory_read_button.Size = new Size(45, 30);
+            memory_read_button.TabIndex = 2;
+            memory_read_button.Text = "MR";
+            memory_read_button.UseVisualStyleBackColor = false;
+            memory_read_button.Click += button2_Click;
+            // 
+            // memory_save_button
+            // 
+            memory_save_button.BackColor = SystemColors.GradientActiveCaption;
+            memory_save_button.Location = new Point(110, 103);
+            memory_save_button.Name = "memory_save_button";
+            memory_save_button.Size = new Size(45, 30);
+            memory_save_button.TabIndex = 3;
+            memory_save_button.Text = "MS";
+            memory_save_button.UseVisualStyleBackColor = false;
+            memory_save_button.Click += button3_Click;
+            // 
+            // memory_add_button
+            // 
+            memory_add_button.BackColor = SystemColors.GradientActiveCaption;
+            memory_add_button.Location = new Point(161, 103);
+            memory_add_button.Name = "memory_add_button";
+            memory_add_button.Size = new Size(45, 30);
+            memory_add_button.TabIndex = 4;
+            memory_add_button.Text = "M+";
+            memory_add_button.UseVisualStyleBackColor = false;
+            memory_add_button.Click += button4_Click;
+            // 
+            // memory_sub_button
+            // 
+            memory_sub_button.BackColor = SystemColors.GradientActiveCaption;
+            memory_sub_button.Location = new Point(212, 103);
+            memory_sub_button.Name = "memory_sub_button";
+            memory_sub_button.Size = new Size(45, 30);
+            memory_sub_button.TabIndex = 5;
+            memory_sub_button.Text = "M-";
+            memory_sub_button.UseVisualStyleBackColor = false;
+            memory_sub_button.Click += button5_Click;
+            // 
+            // erase
+            // 
+            erase.BackColor = SystemColors.GradientActiveCaption;
+            erase.Location = new Point(8, 139);
+            erase.Name = "erase";
+            erase.Size = new Size(45, 30);
+            erase.TabIndex = 10;
+            erase.Text = "<-";
+            erase.UseVisualStyleBackColor = false;
+            erase.Click += erase_Click;
+            // 
+            // clean_cur_num
+            // 
+            clean_cur_num.BackColor = SystemColors.GradientActiveCaption;
+            clean_cur_num.Location = new Point(59, 139);
+            clean_cur_num.Name = "clean_cur_num";
+            clean_cur_num.Size = new Size(45, 30);
+            clean_cur_num.TabIndex = 9;
+            clean_cur_num.Text = "CE";
+            clean_cur_num.UseVisualStyleBackColor = false;
+            clean_cur_num.Click += clean_cur_num_Click;
+            // 
+            // clean_all
+            // 
+            clean_all.BackColor = SystemColors.GradientActiveCaption;
+            clean_all.Location = new Point(110, 139);
+            clean_all.Name = "clean_all";
+            clean_all.Size = new Size(45, 30);
+            clean_all.TabIndex = 8;
+            clean_all.Text = "C";
+            clean_all.UseVisualStyleBackColor = false;
+            clean_all.Click += clean_all_Click;
+            // 
+            // sign
+            // 
+            sign.BackColor = SystemColors.GradientActiveCaption;
+            sign.Location = new Point(161, 139);
+            sign.Name = "sign";
+            sign.Size = new Size(45, 30);
+            sign.TabIndex = 7;
+            sign.Text = "±";
+            sign.UseVisualStyleBackColor = false;
+            sign.Click += sign_Click;
+            // 
+            // sqrt
+            // 
+            sqrt.BackColor = SystemColors.GradientActiveCaption;
+            sqrt.Location = new Point(212, 139);
+            sqrt.Name = "sqrt";
+            sqrt.Size = new Size(45, 30);
+            sqrt.TabIndex = 6;
+            sqrt.Text = "√";
+            sqrt.UseVisualStyleBackColor = false;
+            sqrt.Click += sqrt_Click;
+            // 
+            // seven
+            // 
+            seven.BackColor = SystemColors.GradientInactiveCaption;
+            seven.Location = new Point(8, 175);
+            seven.Name = "seven";
+            seven.Size = new Size(45, 30);
+            seven.TabIndex = 20;
+            seven.Text = "7";
+            seven.UseVisualStyleBackColor = false;
+            seven.Click += seven_Click;
+            // 
+            // eight
+            // 
+            eight.BackColor = SystemColors.GradientInactiveCaption;
+            eight.Location = new Point(59, 175);
+            eight.Name = "eight";
+            eight.Size = new Size(45, 30);
+            eight.TabIndex = 19;
+            eight.Text = "8";
+            eight.UseVisualStyleBackColor = false;
+            eight.Click += seven_Click;
+            // 
+            // nine
+            // 
+            nine.BackColor = SystemColors.GradientInactiveCaption;
+            nine.Location = new Point(110, 175);
+            nine.Name = "nine";
+            nine.Size = new Size(45, 30);
+            nine.TabIndex = 18;
+            nine.Text = "9";
+            nine.UseVisualStyleBackColor = false;
+            nine.Click += seven_Click;
+            // 
+            // div
+            // 
+            div.BackColor = SystemColors.GradientActiveCaption;
+            div.Location = new Point(161, 175);
+            div.Name = "div";
+            div.Size = new Size(45, 30);
+            div.TabIndex = 17;
+            div.Text = "/";
+            div.UseVisualStyleBackColor = false;
+            div.Click += div_Click;
+            // 
+            // percent
+            // 
+            percent.BackColor = SystemColors.GradientActiveCaption;
+            percent.Location = new Point(212, 175);
+            percent.Name = "percent";
+            percent.Size = new Size(45, 30);
+            percent.TabIndex = 16;
+            percent.Text = "%";
+            percent.UseVisualStyleBackColor = false;
+            percent.Click += percent_Click;
+            // 
+            // four
+            // 
+            four.BackColor = SystemColors.GradientInactiveCaption;
+            four.Location = new Point(8, 211);
+            four.Name = "four";
+            four.Size = new Size(45, 30);
+            four.TabIndex = 15;
+            four.Text = "4";
+            four.UseVisualStyleBackColor = false;
+            four.Click += seven_Click;
+            // 
+            // five
+            // 
+            five.BackColor = SystemColors.GradientInactiveCaption;
+            five.Location = new Point(59, 211);
+            five.Name = "five";
+            five.Size = new Size(45, 30);
+            five.TabIndex = 14;
+            five.Text = "5";
+            five.UseVisualStyleBackColor = false;
+            five.Click += seven_Click;
+            // 
+            // six
+            // 
+            six.BackColor = SystemColors.GradientInactiveCaption;
+            six.Location = new Point(110, 211);
+            six.Name = "six";
+            six.Size = new Size(45, 30);
+            six.TabIndex = 13;
+            six.Text = "6";
+            six.UseVisualStyleBackColor = false;
+            six.Click += seven_Click;
+            // 
+            // mul
+            // 
+            mul.BackColor = SystemColors.GradientActiveCaption;
+            mul.Location = new Point(161, 211);
+            mul.Name = "mul";
+            mul.Size = new Size(45, 30);
+            mul.TabIndex = 12;
+            mul.Text = "*";
+            mul.UseVisualStyleBackColor = false;
+            mul.Click += mul_Click;
+            // 
+            // button20
+            // 
+            button20.BackColor = SystemColors.GradientActiveCaption;
+            button20.Location = new Point(212, 211);
+            button20.Name = "button20";
+            button20.Size = new Size(45, 30);
+            button20.TabIndex = 11;
+            button20.Text = "1/x";
+            button20.UseVisualStyleBackColor = false;
+            button20.Click += button20_Click;
+            // 
+            // one
+            // 
+            one.BackColor = SystemColors.GradientInactiveCaption;
+            one.Location = new Point(8, 247);
+            one.Name = "one";
+            one.Size = new Size(45, 30);
+            one.TabIndex = 24;
+            one.Text = "1";
+            one.UseVisualStyleBackColor = false;
+            one.Click += seven_Click;
+            // 
+            // two
+            // 
+            two.BackColor = SystemColors.GradientInactiveCaption;
+            two.Location = new Point(59, 247);
+            two.Name = "two";
+            two.Size = new Size(45, 30);
+            two.TabIndex = 23;
+            two.Text = "2";
+            two.UseVisualStyleBackColor = false;
+            two.Click += seven_Click;
+            // 
+            // three
+            // 
+            three.BackColor = SystemColors.GradientInactiveCaption;
+            three.Location = new Point(110, 247);
+            three.Name = "three";
+            three.Size = new Size(45, 30);
+            three.TabIndex = 22;
+            three.Text = "3";
+            three.UseVisualStyleBackColor = false;
+            three.Click += seven_Click;
+            // 
+            // sub
+            // 
+            sub.BackColor = SystemColors.GradientActiveCaption;
+            sub.Location = new Point(161, 247);
+            sub.Name = "sub";
+            sub.Size = new Size(45, 30);
+            sub.TabIndex = 21;
+            sub.Text = "-";
+            sub.UseVisualStyleBackColor = false;
+            sub.Click += sub_Click;
+            // 
+            // point
+            // 
+            point.BackColor = SystemColors.GradientInactiveCaption;
+            point.Location = new Point(110, 283);
+            point.Name = "point";
+            point.Size = new Size(45, 30);
+            point.TabIndex = 26;
+            point.Text = ",";
+            point.UseVisualStyleBackColor = false;
+            point.Click += point_Click;
+            // 
+            // add
+            // 
+            add.BackColor = SystemColors.GradientActiveCaption;
+            add.Location = new Point(161, 283);
+            add.Name = "add";
+            add.Size = new Size(45, 30);
+            add.TabIndex = 25;
+            add.Text = "+";
+            add.UseVisualStyleBackColor = false;
+            add.Click += add_Click;
+            // 
+            // eq
+            // 
+            eq.BackColor = SystemColors.GradientActiveCaption;
+            eq.Location = new Point(212, 247);
+            eq.Name = "eq";
+            eq.Size = new Size(45, 66);
+            eq.TabIndex = 27;
+            eq.Text = "=";
+            eq.UseVisualStyleBackColor = false;
+            eq.Click += eq_Click;
+            // 
+            // zero
+            // 
+            zero.BackColor = SystemColors.GradientInactiveCaption;
+            zero.Location = new Point(8, 283);
+            zero.Name = "zero";
+            zero.Size = new Size(96, 30);
+            zero.TabIndex = 28;
+            zero.Text = "0";
+            zero.UseVisualStyleBackColor = false;
+            zero.Click += seven_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.GradientInactiveCaption;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(8, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(249, 73);
+            label1.TabIndex = 29;
+            label1.Text = "0";
+            label1.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(266, 324);
+            Controls.Add(label1);
+            Controls.Add(zero);
+            Controls.Add(eq);
+            Controls.Add(point);
+            Controls.Add(add);
+            Controls.Add(one);
+            Controls.Add(two);
+            Controls.Add(three);
+            Controls.Add(sub);
+            Controls.Add(seven);
+            Controls.Add(eight);
+            Controls.Add(nine);
+            Controls.Add(div);
+            Controls.Add(percent);
+            Controls.Add(four);
+            Controls.Add(five);
+            Controls.Add(six);
+            Controls.Add(mul);
+            Controls.Add(button20);
+            Controls.Add(erase);
+            Controls.Add(clean_cur_num);
+            Controls.Add(clean_all);
+            Controls.Add(sign);
+            Controls.Add(sqrt);
+            Controls.Add(memory_sub_button);
+            Controls.Add(memory_add_button);
+            Controls.Add(memory_save_button);
+            Controls.Add(memory_read_button);
+            Controls.Add(memory_clean_button);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Calculator";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button memory_clean_button;
+        private Button memory_read_button;
+        private Button memory_save_button;
+        private Button memory_add_button;
+        private Button memory_sub_button;
+        private Button erase;
+        private Button clean_cur_num;
+        private Button clean_all;
+        private Button sign;
+        private Button sqrt;
+        private Button seven;
+        private Button eight;
+        private Button nine;
+        private Button div;
+        private Button percent;
+        private Button four;
+        private Button five;
+        private Button six;
+        private Button mul;
+        private Button button20;
+        private Button one;
+        private Button two;
+        private Button three;
+        private Button sub;
+        private Button point;
+        private Button add;
+        private Button eq;
+        private Button zero;
+        private Label label1;
+    }
+}
---
+++ calculator/Form1.cs
@@ -0,0 +1,292 @@
+﻿namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public string first_string = "", second_string = "0";
+        public double value = 0, result = 0;
+        public string last_action = "";
+        public double memory = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            memory = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            value = memory;
+            label1.Text = first_string + "\n" + value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            memory = value;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            memory += value;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            memory -= value;
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            if (second_string.Length <= 15 || (second_string[second_string.Length - 1] == ',' && second_string.Length <= 17))
+            {
+                if (second_string == "0")
+                {
+                    second_string = B.Text;
+                }
+                else
+                {
+                    second_string += B.Text;
+                }
+                value = double.Parse(second_string);
+                label1.Text = first_string + "\n" + second_string;
+            }
+        }
+        private void point_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+
+            if (!second_string.Contains(','))
+            {
+                second_string += B.Text;
+            }
+            label1.Text = first_string + "\n" + second_string;
+        }
+
+        private void erase_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            if (second_string != "0") second_string = second_string[..^1];
+            if (second_string.Length == 0) second_string = "0";
+
+            value = double.Parse(second_string);
+            label1.Text = first_string + "\n" + second_string;
+        }
+
+        private void clean_cur_num_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            second_string = "0";
+            label1.Text = first_string + "\n" + second_string;
+        }
+
+        private void clean_all_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            second_string = "0";
+            first_string = "";
+            label1.Text = first_string + "\n" + second_string;
+        }
+
+        private void sign_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            if (second_string != "0")
+            {
+                if (second_string[0] == '-')
+                {
+                    second_string = second_string.Substring(1);
+                }
+                else
+                {
+                    second_string = "-" + second_string;
+                }
+                value *= -1;
+            }
+            label1.Text = first_string + "\n" + second_string;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            if (first_string == "") result = value;
+            else
+            {
+                switch (last_action)
+                {
+                    case "+":
+                        result += value;
+                        break;
+                    case "-":
+                        result -= value;
+                        break;
+                    case "*":
+                        result *= value;
+                        break;
+                    case "/":
+                        result /= value;
+                        if (value == 0) second_string = "Деление на 0!";
+                        break;
+                }
+            }
+
+            last_action = "+";
+            if (second_string != "Деление на 0!") first_string += $"{value.ToString()}+";
+            else first_string = "";
+            if (first_string.Length >= 18) first_string = first_string.Substring(first_string.Length - 18);
+            value = 0;
+            label1.Text = first_string + "\n" + second_string;
+            second_string = "0";
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+            if (first_string == "") result = value;
+            else
+            {
+                switch (last_action)
+                {
+                    case "+":
+                        result += value;
+                        break;
+                    case "-":
+                        result -= value;
+                        break;
+                    case "*":
+                        result *= value;
+                        break;
+                    case "/":
+                        result /= value;
+                        if (value == 0) second_string = "Деление на 0!";
+                        break;
+                }
+            }
+
+            last_action = "-";
+            if (second_string != "Деление на 0!") first_string += $"{value.ToString()}-";
+            else first_string = "";
+            if (first_string.Length >= 18) first_string = first_string.Substring(first_string.Length - 18);
+            value = 0;
+            label1.Text = first_string + "\n" + second_string;
+            second_string = "0";
+        }
+
+        private void mul_Click(object sender, EventArgs e)
+        {
+            if (first_string == "") result = value;
+            else
+            {
+                switch (last_action)
+                {
+                    case "+":
+                        result += value;
+                        break;
+                    case "-":
+                        result -= value;
+                        break;
+                    case "*":
+                        result *= value;
+                        break;
+                    case "/":
+                        result /= value;
+                        if (value == 0) second_string = "Деление на 0!";
+                        break;
+                }
+            }
+
+            last_action = "*";
+            if (second_string != "Деление на 0!") first_string += $"{value.ToString()}*";
+            else first_string = "";
+            if (first_string.Length >= 18) first_string = first_string.Substring(first_string.Length - 18);
+            value = 0;
+            label1.Text = first_string + "\n" + second_string;
+            second_string = "0";
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            if (first_string == "") result = value;
+            else
+            {
+                switch (last_action)
+                {
+                    case "+":
+                        result += value;
+                        break;
+                    case "-":
+                        result -= value;
+                        break;
+                    case "*":
+                        result *= value;
+                        break;
+                    case "/":
+                        result /= value;
+                        if (value == 0) second_string = "Деление на 0!";
+                        break;
+                }
+            }
+
+            last_action = "/";
+            if (second_string != "Деление на 0!") first_string += $"{value.ToString()}/";
+            else first_string = "";
+            if (first_string.Length >= 18) first_string = first_string.Substring(first_string.Length - 18);
+            value = 0;
+            label1.Text = first_string + "\n" + second_string;
+            second_string = "0";
+        }
+
+        private void eq_Click(object sender, EventArgs e)
+        {
+            switch (last_action)
+            {
+                case "+":
+                    result += value;
+                    break;
+                case "-":
+                    result -= value;
+                    break;
+                case "*":
+                    result *= value;
+                    break;
+                case "/":
+                    result /= value;
+                    break;
+            }
+
+            first_string = "";
+            second_string = result.ToString();
+            if (second_string == "∞" || second_string == "-∞") second_string = "Деление на 0!";
+            label1.Text = first_string + "\n" + second_string;
+            value = 0;
+            second_string = "0";
+            result = 0;
+        }
+
+        private void sqrt_Click(object sender, EventArgs e)
+        {
+            value = Math.Sqrt(value);
+            label1.Text = first_string + "\n" + value.ToString();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            value = 1 / value;
+            second_string = value.ToString();
+            if (second_string == "∞" || second_string == "-∞") second_string = "Деление на 0!";
+            label1.Text = first_string + "\n" + second_string;
+        }
+
+        private void percent_Click(object sender, EventArgs e)
+        {
+            value = result / 100 * value;
+            label1.Text = first_string + "\n" + value.ToString();
+        }
+    }
+}
